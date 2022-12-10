39e2cee2d108e51bac29cd5eebf3f1d12ec10de4
osawa-koki <github@osawa.dev>
2022-12-11 00:28:41 +0900

    リファクタリング。

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'simple-grep.ts',
+    title: PROJECT_NAME,
     htmlAttrs: {
       lang: 'ja',
     },
