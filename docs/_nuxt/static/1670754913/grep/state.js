3e3352e34c4025d41cce9d211a4c3b57cbe78b8d
osawa-koki <github@osawa.dev>
2022-12-11 19:41:10 +0900

    デプロイ用ビルド。

--- docs/_nuxt/static/1670754913/grep/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsimple-grep.ts\u002F_nuxt\u002Fstatic\u002F1670754913",layout:"default",error:a,serverRendered:true,routePath:"\u002Fgrep",config:{_app:{basePath:"\u002Fsimple-grep.ts\u002F",assetsPath:"\u002Fsimple-grep.ts\u002F_nuxt\u002F",cdnURL:a}}}}(null));
